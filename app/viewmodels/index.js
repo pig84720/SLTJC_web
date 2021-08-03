2e1c7016f2be0fe05225439d08692ae7d8704521
luke.dong <luke.dong@mail.searching-service.com>
2021-08-03 10:28:18 +0800

    欄位內容塞入HTML

--- app/viewmodels/index.js
+++ app/viewmodels/index.js
@@ -7,121 +7,33 @@ define(function (require) {
         id: '',
         init: function (programId, initVar, callback) {
             var etag = [{
-                "col_01": "2020/11/27 14:58:40",
-                "col_02": "國一北上364-高雄(九如、建國路)-鼎金系統(連接國10、大中路)",
-                "col_03": "北上",
-                "col_04": "5.1"
+                "col_01": "2020/11/27",
+                "col_02": "唱詩祈禱會",
+                "col_03": "撒母耳記上十七章",
+                "col_04": "陳俊安",
+                youtubeUrl: "https://www.youtube.com/watch?v=ulPx0Gcmxcg"
             }, {
-                "col_01": "2020/11/27 15:02:00",
-                "col_02": "國一北上359-鼎金系統(連接國10、大中路)-楠梓(鳳楠路)",
-                "col_03": "北上",
-                "col_04": "6.2"
+                "col_01": "2020/11/28",
+                "col_02": "晚間聚會(三)",
+                "col_03": "甘心奉獻",
+                "col_04": "陳俊安",
+                youtubeUrl: "https://www.youtube.com/watch?v=ulPx0Gcmxcg"
             }, {
-                "col_01": "2020/11/27 15:03:50",
-                "col_02": "國一北上355.9-楠梓(鳳楠路)-楠梓(旗楠路)",
-                "col_03": "北上",
-                "col_04": "2.1"
-            }, {
-                "col_01": "2020/11/27 15:05:49",
-                "col_02": "國一北上352.5-楠梓(旗楠路)-岡山",
-                "col_03": "北上",
-                "col_04": "7.0"
-            }, {
-                "col_01": "2020/11/27 15:09:29",
-                "col_02": "國一北上346-岡山-高科",
-                "col_03": "北上",
-                "col_04": "8.4"
-            }, {
-                "col_01": "2020/11/27 15:13:09",
-                "col_02": "國一北上339.8-高科-路竹",
-                "col_03": "北上",
-                "col_04": "4.9"
-            }, {
-                "col_01": "2020/11/27 15:14:09",
-                "col_02": "國一北上364-高雄(九如、建國路)-鼎金系統(連接國10、大中路)",
-                "col_03": "北上",
-                "col_04": "5.0"
-            }, {
-                "col_01": "2020/11/27 14:58:40",
-                "col_02": "國一北上364-高雄(九如、建國路)-鼎金系統(連接國10、大中路)",
-                "col_03": "北上",
-                "col_04": "5.1"
-            }, {
-                "col_01": "2020/11/27 15:02:00",
-                "col_02": "國一北上359-鼎金系統(連接國10、大中路)-楠梓(鳳楠路)",
-                "col_03": "北上",
-                "col_04": "6.2"
-            }, {
-                "col_01": "2020/11/27 15:03:50",
-                "col_02": "國一北上355.9-楠梓(鳳楠路)-楠梓(旗楠路)",
-                "col_03": "北上",
-                "col_04": "2.1"
-            }, {
-                "col_01": "2020/11/27 15:02:00",
-                "col_02": "國一北上359-鼎金系統(連接國10、大中路)-楠梓(鳳楠路)",
-                "col_03": "北上",
-                "col_04": "6.2"
-            }, {
-                "col_01": "2020/11/27 15:03:50",
-                "col_02": "國一北上355.9-楠梓(鳳楠路)-楠梓(旗楠路)",
-                "col_03": "北上",
-                "col_04": "2.1"
-            }, {
-                "col_01": "2020/11/27 15:05:49",
-                "col_02": "國一北上352.5-楠梓(旗楠路)-岡山",
-                "col_03": "北上",
-                "col_04": "7.0"
-            }, {
-                "col_01": "2020/11/27 15:09:29",
-                "col_02": "國一北上346-岡山-高科",
-                "col_03": "北上",
-                "col_04": "8.4"
-            }, {
-                "col_01": "2020/11/27 15:13:09",
-                "col_02": "國一北上339.8-高科-路竹",
-                "col_03": "北上",
-                "col_04": "4.9"
-            }, {
-                "col_01": "2020/11/27 15:14:09",
-                "col_02": "國一北上364-高雄(九如、建國路)-鼎金系統(連接國10、大中路)",
-                "col_03": "北上",
-                "col_04": "5.0"
-            }, {
-                "col_01": "2020/11/27 14:58:40",
-                "col_02": "國一北上364-高雄(九如、建國路)-鼎金系統(連接國10、大中路)",
-                "col_03": "北上",
-                "col_04": "5.1"
-            }, {
-                "col_01": "2020/11/27 15:02:00",
-                "col_02": "國一北上359-鼎金系統(連接國10、大中路)-楠梓(鳳楠路)",
-                "col_03": "北上",
-                "col_04": "6.2"
-            }, {
-                "col_01": "2020/11/27 15:03:50",
-                "col_02": "國一北上355.9-楠梓(鳳楠路)-楠梓(旗楠路)",
-                "col_03": "北上",
-                "col_04": "2.1"
-            }, {
-                "col_01": "2020/11/27 15:02:00",
-                "col_02": "國一北上359-鼎金系統(連接國10、大中路)-楠梓(鳳楠路)",
-                "col_03": "北上",
-                "col_04": "6.2"
-            }, {
-                "col_01": "2020/11/27 15:03:50",
-                "col_02": "國一北上355.9-楠梓(鳳楠路)-楠梓(旗楠路)",
-                "col_03": "北上",
-                "col_04": "2.1"
+                "col_01": "2020/11/29",
+                "col_02": "晚間聚會(五)",
+                "col_03": "向標竿直跑",
+                "col_04": "陳俊安",
+                youtubeUrl: "https://www.youtube.com/watch?v=ulPx0Gcmxcg"
             }];
             $("#k-grid-etag").kendoGrid({
                 dataSource: {
-                    data: [],
+                    data: etag,
                     schema: {
                         model: {
                             fields: {
                                 col_01: { type: "string" },
                                 col_02: { type: "string" },
-                                col_03: { type: "string" },
-                                col_04: { type: "number" }
+                                col_03: { type: "string" }
                             }
                         }
                     },
@@ -194,28 +106,30 @@ define(function (require) {
                 {
                     field: "col_05",
                     title: "youtube連結",
-                    width: 140,
+                    width: 120,
                     headerAttributes: {
                         "class": "table-header-cell",
                         style: "line-height: 50px"
                     },
                     attributes: {
                       "class": "table-cell",
-                      style: "line-height: 40px; font-size: 15px"
-                    }
+                      style: "line-height: 40px; display: flex; justify-content: center; padding-top: 12px"
+                    },
+                    template: "<div style='width: 30px; height: 30px; background-image: url(../image/2875384.png); background-size: cover; cursor: pointer' onclick='window.open(`#: youtubeUrl #`)'></div>"
                 },
                 {
                     field: "col_06",
                     title: "點擊觀看",
-                    width: 100,
+                    width: 120,
                     headerAttributes: {
                         "class": "table-header-cell",
                         style: "line-height: 50px"
                     },
                     attributes: {
                       "class": "table-cell",
-                      style: "line-height: 40px; font-size: 15px"
-                    }
+                      style: "line-height: 40px; padding-left: 45px"
+                    },
+                    template: "<div style='width: 30px; height: 30px; background-image: url(../image/338113.png); background-size: cover; cursor: pointer' onclick='$(`.modal-vedio-player`).modal(`show`);'></div>"
                 }]
             });
             $('.modal-vedio-player').modal('show');
